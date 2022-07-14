4b15d8676c6e24f8b2e6d957bf36e381906f7a3e
hmbgaq1 <hmbgaq1@gmail.com>
2022-07-14 12:09:19 +0900

    sign in & log in

--- nwitter/src/fbase.js
+++ nwitter/src/fbase.js
@@ -11,13 +11,6 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
     appId: process.env.REACT_APP_APP_ID
-
-    // apiKey: "AIzaSyDUqgirGGrt9sySjqgzABt8aHjZBswpjvw",
-    // authDomain: "nwitter-bc8c0.firebaseapp.com",
-    // projectId: "nwitter-bc8c0",
-    // storageBucket: "nwitter-bc8c0.appspot.com",
-    // messagingSenderId: "908843954209",
-    // appId: "1:908843954209:web:4c8574fb0e7b97278ff360"
   };
 
 firebase.initializeApp(firebaseConfig);
